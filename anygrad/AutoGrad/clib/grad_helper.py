78ba897768e65fd606c407c62a0ca595a71540f1
Ruhaan18 <raiyandalal8@gmail.com>
2024-12-29 11:04:30 +0530

    add some grad helper

--- anygrad/AutoGrad/clib/grad_helper.py
+++ anygrad/AutoGrad/clib/grad_helper.py
@@ -1,85 +1,66 @@
 import anygrad
 class GradientCal:
     @staticmethod
+    def initialize_grad(tensor):
+        """Ensure tensor.grad is initialized."""
+        if tensor.grad is None:
+            tensor.grad = anygrad.zeros_like(tensor, requires_grad=False)
+    
+    @staticmethod
+    def accumulate_grad_with_factor(tensor, factor):
+        """Accumulate gradients with a scaling factor."""
+        if tensor.requires_grad:
+            GradientCal.initialize_grad(tensor)
+            GradientCal.accumulate_grad(tensor, factor)
+
+    @staticmethod
     def accumulate_grad(tensor1, tensor2):
+        """Accumulate gradients for tensor1 using tensor2."""
         if tensor1.grad.shape != tensor2.shape:
             tensor2.grad = tensor2.sum(axis=0, keepdims=True)
         tensor1.grad += tensor2
-    
+
     @staticmethod
     def Add_grad(tensor1, tensor2, ans_tensor):
         def _backward():
-            if tensor1.requires_grad:
-                if tensor1.grad is None:
-                   tensor1.grad = anygrad.zeros_like(tensor1, requires_grad=False) 
-                GradientCal.accumulate_grad(tensor1, ans_tensor.grad)
-                
+            GradientCal.accumulate_grad_with_factor(tensor1, ans_tensor.grad)
             if not isinstance(tensor2, (int, float)):
-                if tensor2.requires_grad:
-                    if tensor2.grad is None:
-                        tensor2.grad = anygrad.zeros_like(tensor2, requires_grad=False)
-                    GradientCal.accumulate_grad(tensor2, ans_tensor.grad)
+                GradientCal.accumulate_grad_with_factor(tensor2, ans_tensor.grad)
         return _backward
-    
+
     @staticmethod
-    def Sub_grad(tenosr1, tensor2, ans_tensor):
+    def Sub_grad(tensor1, tensor2, ans_tensor):
         def _backward():
-            if tenosr1.requires_grad:
-                if tenosr1.grad is None:
-                    tenosr1.grad = anygrad.zeros_like(tenosr1, requires_grad=False)
-                GradientCal.accumulate_grad(tenosr1, ans_tensor.grad)
-                
+            GradientCal.accumulate_grad_with_factor(tensor1, ans_tensor.grad)
             if not isinstance(tensor2, (int, float)):
-                if tensor2.requires_grad:
-                    if tensor2.grad is None:
-                        tensor2.grad = anygrad.zeros_like(tenosr1, requires_grad=False)
-                GradientCal.accumulate_grad(tensor2, ans_tensor.grad * -1)
+                GradientCal.accumulate_grad_with_factor(tensor2, -ans_tensor.grad)
         return _backward
 
     @staticmethod
     def Mul_grad(tensor1, tensor2, ans_tensor):
         def _backward():
             if tensor1.requires_grad:
-                if tensor1.grad is None:
-                    tensor1.grad = anygrad.zeros_like(tensor1, requires_grad=False)
-                acc = tensor2 * ans_tensor.grad
-                acc.requires_grad = False
-                GradientCal.accumulate_grad(tensor1, acc)
-                
-            if not isinstance(tensor2, (int, float)):
-                if tensor2.requires_grad:
-                    if tensor2.grad is None:
-                        tensor2.grad = anygrad.zeros_like(tensor2, requires_grad=False)
-                    acc = tensor1 * ans_tensor.grad
-                    acc.requires_grad = False
-                    GradientCal.accumulate_grad(tensor2, acc)  
-            else:
-                if tensor1.requires_grad:
-                    if tensor1.grad is None:
-                        tensor1.grad = anygrad.zeros_like(tensor2, requires_grad=False)
-                    acc = tensor2 * ans_tensor.grad
-                    acc.requires_grad = False
-                    GradientCal.accumulate_grad(tensor1, acc)
-                             
+                GradientCal.accumulate_grad_with_factor(tensor1, tensor2 * ans_tensor.grad)
+            if not isinstance(tensor2, (int, float)) and tensor2.requires_grad:
+                GradientCal.accumulate_grad_with_factor(tensor2, tensor1 * ans_tensor.grad)
         return _backward
-    
+
     @staticmethod
     def Div_grad(tensor1, tensor2, ans_tensor):
         def _backward():
-            if tensor1.grad is None:
-                tensor1.grad = anygrad.zeros_like(tensor1, requires_grad=False)
-            acc = (1 / tensor2) * ans_tensor.grad
-            acc.requires_grad = False
-            GradientCal.accumulate_grad(tensor1, acc)
+            if tensor1.requires_grad:
+                GradientCal.accumulate_grad_with_factor(tensor1, (1 / tensor2) * ans_tensor.grad)
+            if not isinstance(tensor2, (int, float)) and tensor2.requires_grad:
+                GradientCal.accumulate_grad_with_factor(tensor2, ((-tensor1) / tensor2**2) * ans_tensor.grad)
+        return _backward
 
-            if not isinstance(tensor2, (int, float)):
-                if tensor2.requires_grad:
-                    if tensor2.grad is None:
-                        tensor2.grad = anygrad.zeros_like(tensor2, requires_grad=False)
-                    acc = ((-tensor1) / tensor2 ** 2) * ans_tensor.grad
-                    acc.requires_grad = False
-                    GradientCal.accumulate_grad(tensor2, acc)
-                    
+    @staticmethod
+    def Pow_grad(tensor1, tensor2, ans_tensor):
+        def _backward():
+            if tensor1.requires_grad:
+                GradientCal.accumulate_grad_with_factor(tensor1, tensor2 * ans_tensor.grad * (ans_tensor / tensor1))
+            if not isinstance(tensor2, (int, float)) and tensor2.requires_grad:
+                GradientCal.accumulate_grad_with_factor(tensor2, ans_tensor.grad * ans_tensor * anygrad.log(tensor1))
         return _backward
 
     @staticmethod
