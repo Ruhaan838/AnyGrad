ef60659a3686344a88937daf33a707a30cdca7ac
Ruhaan18 <raiyandalal8@gmail.com>
2025-03-09 17:04:34 +0530

    simple formateing

--- anygrad/__init__.py
+++ anygrad/__init__.py
@@ -2,23 +2,57 @@ from .tensor.tensor import Tensor
 from .tensor.floattensor import FloatTensor
 from .tensor.inttensor import IntTensor
 from .tensor.booltensor import BoolTensor
-from .tensor.ThHelper import (float32, float64, 
-                              int32, int64, 
-                              bool)
+from .tensor.ThHelper import float32, float64, int32, int64, bool
 from .AutoGrad import no_grad
-from .utils import (Generator, rand, randint, 
-                    ones, ones_like, 
-                    zeros, zeros_like, 
-                    log, log10, log2, exp, exp2)
+from .utils import (
+    Generator,
+    rand,
+    randint,
+    ones,
+    ones_like,
+    zeros,
+    zeros_like,
+    log,
+    log10,
+    log2,
+    exp,
+    exp2,
+)
 from .version import __version__
+
 
 def matmul(tensor1, tensor2):
     return tensor1 @ tensor2
 
-def cast(tensor:Tensor, target_dtype):
+
+def cast(tensor: Tensor, target_dtype):
     return Tensor(tensor.data, requires_grad=tensor.requires_grad, dtype=target_dtype)
 
-__all__ = ["Tensor", "FloatTensor", "IntTensor", "BoolTensor",
-           "float32", "float64", "int32", "int64", "bool",
-           "no_grad", "Generator", "rand", "randint", "ones", "ones_like", "zeros", "zeros_like",
-           "log", "log10", "log2", "exp", "exp2", "matmul", "cast", "__version__"]
+
+__all__ = [
+    "Tensor",
+    "FloatTensor",
+    "IntTensor",
+    "BoolTensor",
+    "float32",
+    "float64",
+    "int32",
+    "int64",
+    "bool",
+    "no_grad",
+    "Generator",
+    "rand",
+    "randint",
+    "ones",
+    "ones_like",
+    "zeros",
+    "zeros_like",
+    "log",
+    "log10",
+    "log2",
+    "exp",
+    "exp2",
+    "matmul",
+    "cast",
+    "__version__",
+]
--- anygrad/version.py
+++ anygrad/version.py
@@ -1,11 +1,11 @@
-
 class Version(str):
-    def __init__(self, version:str):
+    def __init__(self, version: str):
         self.version = version
-    
+
     def __repr__(self):
         return f"Version(anygrad={self.version})"
-    
+
     __module__ = "anygrad"
-    
-__version__ = Version('0.0.2')
+
+
+__version__ = Version("0.0.2")
